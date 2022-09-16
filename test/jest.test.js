70682c569b5bbd08663fc10480235f5d6bf9c60f
neuroticks <neuroticks@gmail.com>
2022-09-16 20:29:38 -0300

    server respondendo na porta 3001 respondendo na raiz teste passou

--- test/jest.test.js
+++ test/jest.test.js
@@ -1,32 +1,31 @@
-
 test('devo conhecer as principais assertivas do JEST', () => {
-    let numero = null
-    expect(numero).toBeNull()
+  let numero = null;
+  expect(numero).toBeNull();
 
-    numero = 10
-    expect(numero).not.toBeNull()
+  numero = 10;
+  expect(numero).not.toBeNull();
 
-    expect(numero).toBe(10)
+  expect(numero).toBe(10);
 
-    expect(numero).toEqual(10)
+  expect(numero).toEqual(10);
 
-    expect(numero).toBeGreaterThan(9)
+  expect(numero).toBeGreaterThan(9);
 
-    expect(numero).toBeLessThan(11)
-})
+  expect(numero).toBeLessThan(11);
+});
 
 test('Devo saber trabalhar com objetos', () => {
-    const obj = {name: 'Rodrigo', mail: 'rod@mail.com'}
+  const obj = { name: 'Rodrigo', mail: 'rod@mail.com' };
 
-    expect(obj).toHaveProperty('name')
+  expect(obj).toHaveProperty('name');
 
-    expect(obj).toHaveProperty('name', 'Rodrigo')
+  expect(obj).toHaveProperty('name', 'Rodrigo');
 
-    expect(obj.name).toBe('Rodrigo')
+  expect(obj.name).toBe('Rodrigo');
 
-    const obj2 = {name: 'Rodrigo', mail: 'rod@mail.com'}
+  const obj2 = { name: 'Rodrigo', mail: 'rod@mail.com' };
 
-    expect(obj).toEqual(obj2) // é igual porque tem os mesmos valores
+  expect(obj).toEqual(obj2); // é igual porque tem os mesmos valores
 
-    expect(obj).not.toBe(obj2) // mas não é o mesmo obj porque são duas vars distintas
-})
+  expect(obj).not.toBe(obj2); // mas não é o mesmo obj porque são duas vars distintas
+});
