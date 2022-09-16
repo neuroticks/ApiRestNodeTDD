f76d38c07de5eb900da85dbd4678103a97f73d22
neuroticks <neuroticks@gmail.com>
2022-09-16 20:14:07 -0300

    Test - servidor deve responder na porta 3001

---
+++ test/server.test.js
@@ -0,0 +1,9 @@
+const supertest = require('supertest')
+
+const request = supertest('http://localhost:3001')
+
+test('Deve responder na porta 3001', async ()=>{
+    // acessar a url http://localhost:3001
+    const res = await request.get('/')
+    return expect(res.status).toBe(200)
+})
