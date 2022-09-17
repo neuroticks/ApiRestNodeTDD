2ab914bdb1477caddae200647755570d7871355f
neuroticks <neuroticks@gmail.com>
2022-09-16 23:03:00 -0300

    Refatoração: Utilizando Consign Auto loading files - middleware (body-parse)

--- test/user.test.js
+++ test/user.test.js
@@ -1,20 +1,16 @@
-const request = require('supertest')
+const request = require('supertest');
 
-const app = require('../src/app')
+const app = require('../src/app');
 
-test('Deve listar todos os usuários', ()=>{
-    return request(app).get('/users')
-    .then((res) =>{
-        expect(res.status).toBe(200)
-        expect(res.body[0]).toHaveProperty('nome','rodrigo')
-    })
-})
+test('Deve listar todos os usuários', () => request(app).get('/users')
+  .then((res) => {
+    expect(res.status).toBe(200);
+    expect(res.body[0]).toHaveProperty('nome', 'rodrigo');
+  }));
 
-test('Deve inserir usuário',()=>{
-    return request(app).post('/users')
-    .send({nome:'Zebra', email:'zebra@mail.com'})
-    .then((res)=>{
-        expect(res.status).toBe(201)
-        expect(res.body.nome).toBe('Zebra')
-    })
-})
+test('Deve inserir usuário', () => request(app).post('/users')
+  .send({ nome: 'Zebra', email: 'zebra@mail.com' })
+  .then((res) => {
+    expect(res.status).toBe(201);
+    expect(res.body.nome).toBe('Zebra');
+  }));
