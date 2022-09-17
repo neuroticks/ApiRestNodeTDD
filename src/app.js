32d85271d22a5dc3bb8b0ec9bb1e9c2093523963
neuroticks <neuroticks@gmail.com>
2022-09-16 21:10:05 -0300

    Deve listar usuario Teste passou

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
-const app = require('express')();
+const app = require("express")();
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.status(200).send();
+});
+
+app.get("/users", (req, res) => {
+  const users = [{ nome: "rodrigo", email: "rodrigo@mail.com" }];
+  res.status(200).json(users);
 });
 
 module.exports = app;
