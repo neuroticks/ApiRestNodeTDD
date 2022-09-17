8194dc04123cabbe9d9f1d25bc7ff9b5f4b53b2c
neuroticks <neuroticks@gmail.com>
2022-09-17 17:20:39 -0300

    ajuste do lint

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,15 +1,14 @@
 module.exports = () => {
+  const findAll = (req, res) => {
+    const users = [
+      { nome: 'rodrigo', email: 'rodrigo@mail.com' },
+    ];
+    res.status(200).json(users);
+  };
 
-    const findAll = (req, res)=>{
-        const users = [
-            { nome: "rodrigo", email: "rodrigo@mail.com" }
-        ];
-        res.status(200).json(users);
-      }
-      
-      const create=(req, res)=>{
-        res.status(201).json(req.body)
-      }
-      
-      return { findAll, create }
-}
+  const create = (req, res) => {
+    res.status(201).json(req.body);
+  };
+
+  return { findAll, create };
+};
