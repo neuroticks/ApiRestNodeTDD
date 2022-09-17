5c56719e8d0f08746ec340abbc4d65b7dc4bbd75
neuroticks <neuroticks@gmail.com>
2022-09-17 17:40:06 -0300

    knex: cria tabela users comando (node_modules/.bin/knex migrate:latest --env test)

--- src/migrations/20220917202338_create_users.js
+++ src/migrations/20220917202338_create_users.js
@@ -2,14 +2,24 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema
+    .createTable(
+      'users',
+      (t) => {
+        t.increments('id').primary();
+        t.string('nome').notNullable();
+        t.string('email').notNullable().unique();
+        t.string('senha').notNullable();
+      },
+    );
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema
+    .dropTable('users');
 };
