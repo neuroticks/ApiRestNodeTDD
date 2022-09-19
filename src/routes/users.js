dae48d44db28e7ebfde033be2065d44b2d2c7709
neuroticks <neuroticks@gmail.com>
2022-09-19 11:35:57 -0300

    Rota de user integrada com o banco de dados

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,13 +1,14 @@
-module.exports = () => {
+const app = require('../app');
+
+module.exports = (app) => {
   const findAll = (req, res) => {
-    const users = [
-      { nome: 'rodrigo', email: 'rodrigo@mail.com' },
-    ];
-    res.status(200).json(users);
+    app.db('users').select()
+      .then((result) => res.status(200).json(result));
   };
 
-  const create = (req, res) => {
-    res.status(201).json(req.body);
+  const create = async (req, res) => {
+    const result = await app.db('users').insert(req.body, '*');
+    res.status(201).json(result[0]);
   };
 
   return { findAll, create };
