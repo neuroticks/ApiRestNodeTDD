fc0f27de829d51c0188bb790ec7c0fb819ec46de
neuroticks <neuroticks@gmail.com>
2022-09-19 13:41:14 -0300

    Service [user] - módulo [user] na camada [services]

---
+++ src/services/user.js
@@ -0,0 +1,12 @@
+const app = require("../app");
+
+module.exports=(app) =>{
+    const findAll = () => {
+        return app.db('users').select()
+    }
+    const save = (user) => {
+        return app.db('users').insert(user, '*')
+    }
+
+    return {findAll, save}
+}
