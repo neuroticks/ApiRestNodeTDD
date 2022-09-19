74ae625859db9d5d81282379730e410e647c8508
neuroticks <neuroticks@gmail.com>
2022-09-19 11:48:33 -0300

    Verificar LOG do knex através de seus eventos. Muito útil para debugar a procura de algum erro nesta conexão com o BD.

--- src/app.js
+++ src/app.js
@@ -2,12 +2,12 @@ const app = require('express')();
 const consign = require('consign');
 const knex = require('knex');
 const knexfile = require('../knexfile');
-const knexLogger = require('knex-logger')
+//const knexLogger = require('knex-logger')
 
 // TODO: criar chaveamento dinâmico
 app.db = knex(knexfile.test);
 
-app.use(knexLogger(app.db))
+//app.use(knexLogger(app.db))
 
 consign({ cwd: 'src', verbose: false })
   .include('./config/middleware.js')
@@ -18,5 +18,13 @@ consign({ cwd: 'src', verbose: false })
 app.get('/', (req, res) => {
   res.status(200).send();
 });
+
+
+app.db.on('query', (query)=>{
+  console.log({sql:query.sql, binding: query.bindings ? query.bindings : ''})
+})
+  .on('query-response', response => console.log(response))
+  .on('error', error => console.log(error))
+
 
 module.exports = app;
