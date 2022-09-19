0b3017638498184b992f6f5c075c986fe109a3ae
neuroticks <neuroticks@gmail.com>
2022-09-19 11:34:57 -0300

    Testes readequados para o novo formato com banco de dados

--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -5,12 +5,15 @@ const app = require('../../src/app');
 test('Deve listar todos os usuários', () => request(app).get('/users')
   .then((res) => {
     expect(res.status).toBe(200);
-    expect(res.body[0]).toHaveProperty('nome', 'rodrigo');
+    expect(res.body.length).toBeGreaterThan(0);
   }));
 
-test.skip('Deve inserir usuário', () => request(app).post('/users')
-  .send({ nome: 'Zebra', email: 'zebra@mail.com' })
+test('Deve inserir usuário', () => {
+  const email = `${Date.now()}@email.com.br`
+  return request(app).post('/users')
+  .send({ nome: 'Zebra', email, senha: 'senha' })
   .then((res) => {
     expect(res.status).toBe(201);
     expect(res.body.nome).toBe('Zebra');
-  }));
+  })
+});
