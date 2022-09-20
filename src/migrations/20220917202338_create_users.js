bab5d4a9c083c4f1c257d54b5611a6b0bd5a382e
neuroticks <neuroticks@gmail.com>
2022-09-20 09:52:30 -0300

    LINT: Ajuste - ajuste lint (função sem nome)

--- src/migrations/20220917202338_create_users.js
+++ src/migrations/20220917202338_create_users.js
@@ -2,24 +2,15 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function (knex) {
-  return knex.schema
-    .createTable(
-      'users',
-      (t) => {
-        t.increments('id').primary();
-        t.string('nome').notNullable();
-        t.string('email').notNullable().unique();
-        t.string('senha').notNullable();
-      },
-    );
-};
+exports.up = (knex) => knex.schema.createTable('users', (t) => {
+  t.increments('id').primary();
+  t.string('nome').notNull();
+  t.string('email').notNull().unique();
+  t.string('senha').notNull();
+});
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function (knex) {
-  return knex.schema
-    .dropTable('users');
-};
+exports.down = (knex) => knex.schema.dropTable('users');
