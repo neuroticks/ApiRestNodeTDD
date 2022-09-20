77565411500b718225e48210ba926e775ee9970d
neuroticks <neuroticks@gmail.com>
2022-09-20 12:59:58 -0300

    TEST [accounts] findAll - deve listar todas as contas

--- test/routes/account.test.js
+++ test/routes/account.test.js
@@ -5,12 +5,14 @@ const app = require('../../src/app');
 const ROTA_DE_CONTAS = '/accounts';
 let usuario;
 
+// #region Antes dos Testes
 beforeAll(async () => {
   const res = await app.services.user
     .save({ nome: 'Nome Usuário Account', email: `${Date.now()}@email.com`, senha: '123' });
 
   usuario = { ...res[0] };
 });
+// #endregion Antes dos Testes
 
 const nomeContaTeste = 'Conta Testes #1';
 
@@ -19,4 +21,12 @@ test('Deve inserir uma conta para um usuário', () => request(app).post(ROTA_DE_
   .then((retorno) => {
     expect(retorno.status).toBe(201);
     expect(retorno.body.nome).toBe(nomeContaTeste);
+  }));
+
+test('Deve listar todas as contas', () => app.db('accounts')
+  .insert({ nome: nomeContaTeste, user_id: usuario.id })
+  .then(() => request(app).get(ROTA_DE_CONTAS))
+  .then((res) => {
+    expect(res.status).toBe(200);
+    expect(res.body.length).toBeGreaterThan(0);
   }));
